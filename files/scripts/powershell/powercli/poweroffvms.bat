f130e116029fe64768309a18e18eaec3c0ce9360
Lee Johnson <lee.james.johnson@gmail.com>
2022-03-22 13:13:59 -0400

    group updates to public branch

--- files/scripts/powershell/powercli/poweroffvms.bat
+++
@@ -1,1 +0,0 @@
-powershell -psc "C:\Program Files (x86)\VMware\Infrastructure\vSphere PowerCLI\vim.psc1" -file "C:\apps\powershell\powercli\poweroffvms.ps1"
--- files/scripts/powershell/setup-dev-env.cmd
+++
@@ -1,3 +0,0 @@
-powershell %USERPROFILE%\.setup-dev-env.ps1 >> “%USERPROFILE%\setup-dev-env.log”
-rem powershell -windowstyle hidden %USERPROFILE%\.setup-dev-env.ps1 >> “%USERPROFILE%\setup-dev-env.log”
-
