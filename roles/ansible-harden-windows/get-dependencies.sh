6baf3ea1dd2cc86393f49e3d95ac0aa9c81e5fa8
Lee Johnson <lee.james.johnson@gmail.com>
2022-05-06 16:27:52 -0400

    group updates to public branch

---
+++ roles/ansible-harden-windows/get-dependencies.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+## one script to be used by travis, jenkins, packer...
+
+umask 022
+
+rolesdir="$(dirname $0)/.."
+[ $APPVEYOR -o $CI ] && rolesdir="/etc/ansible/roles"
+
+[ ! -d $rolesdir/juju4.win-firewall ] && git clone https://github.com/juju4/ansible-win-firewall $rolesdir/juju4.win-firewall
+[ ! -d $rolesdir/juju4.win-eventlog ] && git clone https://github.com/juju4/ansible-win-eventlog $rolesdir/juju4.win-eventlog
+[ ! -d $rolesdir/juju4.win-audit ] && git clone https://github.com/juju4/ansible-win-audit $rolesdir/juju4.win-audit
+[ ! -d $rolesdir/juju4.win-powershell ] && git clone https://github.com/juju4/ansible-win-powershell $rolesdir/juju4.win-powershell
+[ ! -d $rolesdir/juju4.win-sysmon ] && git clone https://github.com/juju4/ansible-win-sysmon $rolesdir/juju4.win-sysmon
+[ ! -d $rolesdir/juju4.win-applocker ] && git clone https://github.com/juju4/ansible-win-applocker $rolesdir/juju4.win-applocker
+[ ! -d $rolesdir/juju4.win-services ] && git clone https://github.com/juju4/ansible-win-services $rolesdir/juju4.win-services
+[ ! -d $rolesdir/juju4.win-msoffice ] && git clone https://github.com/juju4/ansible-win-msoffice $rolesdir/juju4.win-msoffice
+[ ! -d $rolesdir/juju4.win-aptsimulator ] && git clone https://github.com/juju4/ansible-win-aptsimulator $rolesdir/juju4.win-aptsimulator
+[ ! -d $rolesdir/juju4.win-atomic-red-team ] && git clone https://github.com/juju4/ansible-win-atomic-red-team $rolesdir/juju4.win-atomic-red-team
+[ ! -d $rolesdir/juju4.win-osquery ] && git clone https://github.com/juju4/ansible-win-osquery $rolesdir/juju4.win-osquery
+[ ! -d $rolesdir/juju4.win-ad-monitor ] && git clone https://github.com/juju4/ansible-win-ad-monitor $rolesdir/juju4.win-ad-monitor
+[ ! -d $rolesdir/juju4.harden_windows ] && git clone https://github.com/juju4/ansible-harden-windows $rolesdir/juju4.harden_windows
+
+## don't stop build on this script return code
+true
