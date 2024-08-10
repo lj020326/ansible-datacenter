20e9b0ddc7e61dbe5a2a6b1bd3e37db62ee86b4e
Lee Johnson <lee.james.johnson@gmail.com>
2024-08-10 09:14:38 -0400

    group updates to public branch

--- playbooks/create_symlinks.sh
+++ playbooks/create_symlinks.sh
@@ -10,24 +10,24 @@ find . -maxdepth 1 -type l -print -exec rm {} \;
 echo "==> Set up site.yml symlink"
 ln -sf ../site.yml
 
-echo "==> Set up collections symlink"
-ln -sf ../collections
+#echo "==> Set up collections symlink"
+#ln -sf ../collections
+#
+#echo "==> Set up functionality symlinks (library/filter_plugins)"
+##ln -sf ../library
+#ln -sf ../filter_plugins
+#ln -sf ../plugins
 
-echo "==> Set up functionality symlinks (library/filter_plugins)"
-#ln -sf ../library
-ln -sf ../filter_plugins
-ln -sf ../plugins
-
-echo "==> Set up roles and vars symlinks (roles/vars)"
-ln -sf ../roles
-ln -sf ../vars
+#echo "==> Set up roles and vars symlinks (roles/vars)"
+#ln -sf ../roles
+#ln -sf ../vars
 
 #echo "==> Set up resource/other symlinks (molecule/files/etc)"
 #ln -sf ../molecule
 ##ln -sf ../files
 
-echo "==> Set up inventory symlink"
-#ln -sf ../inventory/dev inventory
-ln -sf ../inventory
+#echo "==> Set up inventory symlink"
+##ln -sf ../inventory/dev inventory
+#ln -sf ../inventory
 
 echo "==> Finished"
